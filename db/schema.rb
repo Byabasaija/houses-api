ee8ed8b2c8560f50f8ed2c2c12a9ab10174ab942
Byabasaija <basaijapascal9@gmail.com>
2021-07-20 11:54:21 +0300

    fix migration relations

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_133809) do
+ActiveRecord::Schema.define(version: 2021_07_20_085259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "favorites", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "house_id"
+    t.bigint "user_id", null: false
+    t.bigint "house_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["house_id"], name: "index_favorites_on_house_id"
@@ -40,4 +40,6 @@ ActiveRecord::Schema.define(version: 2021_07_19_133809) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "favorites", "houses"
+  add_foreign_key "favorites", "users"
 end
