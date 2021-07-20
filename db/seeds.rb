ca527f0bf7ece3ece6d396a3d9dfc4505be8fa87
Byabasaija <basaijapascal9@gmail.com>
2021-07-20 12:53:44 +0300

    add seeds to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do
+    house = House.create(name: Faker::Lorem.word, image_url: 'https://unsplash.com/photos/kUdbEEMcRwE', description: '2100 Club, the world’s first blockchain bar, designed by LYCS Architecture.The bar is participated by many well-known entrepreneurs and cornerstone investors in the Blockchain industry. 2100 Club is a social platform that shares and exchanges the cutting edge blockchain technologies, researches and investments. The slogan of 2100Club shares the idea ‘to own, to live to live until 2100’ to all the new economic drivers and innovators. It has been further developed as the basis design concept of interior space which interpreting its intellectual spirit instead of creating a theme bar.')
+  end
