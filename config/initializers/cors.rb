f38265de58cf4d203c5137f694f438846371d51c
Byabasaija <basaijapascal9@gmail.com>
2021-07-28 00:47:30 +0300

    resolve cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,7 +16,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins 'https://find-your-houses.herokuapp.com/'
+    origins 'https://find-your-houses.herokuapp.com'
 
     resource '*',
       headers: :any,
