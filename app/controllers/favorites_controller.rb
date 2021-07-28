edd043f2b92e700bc053ff5953b23af7a9e05ef3
Byabasaija <basaijapascal9@gmail.com>
2021-07-28 17:57:34 +0300

    debug

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,8 @@
 class FavoritesController < ApplicationController
   skip_before_action :verify_authenticity_token
-  include CurrentUserConcern
+ 
   def create
+    @current_user = User.find(params[:user_id])
     @house = House.find(params[:house_id])
     @favorite = Favorite.create!(user_id: @current_user.id, house_id: @house.id)
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:create, :show, :destroy]
   resources :registrations, only: [:create]
-  resources :houses do
-    resources :favorites, only: [:create]
-  end
+  resources :houses 
+   
   resources :favorites, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
