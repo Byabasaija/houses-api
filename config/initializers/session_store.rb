5bc20e3eb0d56f7cffe911c9b078bb9be913456b
Byabasaija <basaijapascal9@gmail.com>
2021-07-28 16:31:38 +0300

    debug

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 
 #config/initializers/session_store.rb
 if Rails.env === 'production' 
-    Rails.application.config.session_store :cookie_store, key: '_houses-api', domain: 'https://houses-api1.herokuapp.com/'
+    Rails.application.config.session_store :cookie_store, key: '_houses-api1', domain: 'https://houses-api1.herokuapp.com/'
   else
-Rails.application.config.session_store :cookie_store, key: '_houses-api'
+Rails.application.config.session_store :cookie_store, key: '_houses-api1'
   end
